baa427e850659f226a030ec0d8de13e64a4be49a
Asafe <asafeluiztoschi@gmail.com>
2020-04-25 12:07:04 -0300

    Piano v1.0
    - All tile sound add
    - new feature add

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-const notas = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"] // Notas
+const notas = [
+    "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"
+]
 
 const escala = [
     "4",
     "5"
 ]
 keyList = [
-    "65", "87", "83", "69", "68", "70", "84", "71", "89", "72", "85", "74", "75", "79", "76", "80", "186", "222", "221", "220", "100", "103", "101", "104"
+    "z", "s", "x", "d", "c", "v", "g", "b", "h", "n", "j", "m", "q", "2", "w", "3", "e", "r", "5", "t", "6", "y", "7", "u"
 ]
+keyListLower = [
+    "Z", "S", "X", "D", "C", "V", "G", "B", "H", "N", "J", "M", "Q", "@", "W", "#", "E", "R", "%", "T", "¨", "Y", "&", "U"
+]
+
 
 const sound = []
 escala.forEach(escala => {
     notas.forEach((nota, i) => {
         sound.push(new Audio("media/" + nota + escala + ".mp3"))
-
-
     })
 })
 
+var teste
 var i = 0
 $("#piano div[id^=escala_] div.tile").each(function (index) {
     if( index % 12 == 0 ) {
@@ -29,43 +34,76 @@ $("#piano div[id^=escala_] div.tile").each(function (index) {
     $(this).attr("id", nota)
     
     $(this).click(function(){
-        sound[index].play()
-        setTimeout(function(){
-            sound[index].pause()
-            sound[index].currentTime = 0
-        },1500)
+        n = sound[index].cloneNode()
+        n.play()
     })
 
-    $(document).keydown(function (){
+    pressedKey = []
+    $(document).keydown(function (eventKey){
+        play = true
+
         keyList.forEach((key, index) => {
-            if ( event.which == key ) {
-                event.preventDefault()
-                sound[index].play()
-                idElement = sound[0].src.split("/")[6].split(".")[0]
-                // alert(idElement)
-                $("#" + idElement).addClass("active")
+            if ( eventKey.key === key ) {
+                
+                pressedKey.forEach(function(pressedKey){
+                    if (key === pressedKey) {
+                        play = false
+                    }
+                })
+
+
+                if(play == true){
+                    pressedKey.push(key)
+
+                    event.preventDefault()
+                    var n = sound[index].cloneNode()
+                    n.play()
+                    var idElement = sound[index].src.split("/")[6].split(".")[0]
+                    $("#" + idElement).addClass("active")
+                }
             }
         })
+        keyListLower.forEach((key, index) => {
+            if ( eventKey.key === key ) {
+                
+                pressedKey.forEach(function(pressedKey){
+                    if (key === pressedKey) {
+                        play = false
+                    }
+                })
 
-    }).keyup(function (){
-        keyList.forEach((key, index) => {
-            if ( event.which == key ) {
-                event.preventDefault()
-                setTimeout(function(){
-                    sound[index].pause()
-                    sound[index].currentTime = 0
-                }, 800)
+
+                if(play == true){
+                    pressedKey.push(key)
+
+                    event.preventDefault()
+                    var n = sound[index].cloneNode()
+                    n.volume = 0.35
+                    n.playbackRate = 2
+                    n.play()
+                    var idElement = sound[index].src.split("/")[6].split(".")[0]
+                    $("#" + idElement).addClass("active")
+                }
             }
+        })
+    }).keyup(function (eventKey){
+        keyList.forEach((key, index) => {
+            indexToRemove = pressedKey.findIndex(arrayItem => arrayItem === key)
+            pressedKey.splice(indexToRemove, 1)
+
+
+            var idElement = sound[index].src.split("/")[6].split(".")[0]
+            $("#" + idElement).removeClass("active")
+        })
+
+        keyListLower.forEach((key, index) => {
+            indexToRemove = pressedKey.findIndex(arrayItem => arrayItem === key)
+            pressedKey.splice(indexToRemove, 1)
+
+
+            var idElement = sound[index].src.split("/")[6].split(".")[0]
+            $("#" + idElement).removeClass("active")
         })
     })
     i++
 })
-
-
-// // Play note sound
-// audio = new Audio("media/5/piano-B5.wav")
-// audio.play() 
-
-// // Stop note sound
-// audio.pause()
-// audio.currentTime = 0
